4409cbb3a707257f832c0cfbfc60c77a700d89cc
Vinicius-Merces <92902670+Vinicius-Merces@users.noreply.github.com>
2022-07-04 23:50:05 -0300

    Update script.js

--- Java/script.js
+++ Java/script.js
@@ -1,1 +1,42 @@
-alert("Olá meus queridos!")
+// Boolean permite fazer operações lógicas (True e False)
+// Null é vazio
+// Undefined 
+// Number
+// String é uma cadeia de caracteres
+// + Adição
+// - Subtração
+// * Multiplicação
+//  / Divisão
+// % Módulo é para pegar o que resta da divisão, não gera número quebrado na divisão
+// ++ incremento
+// -- Decremento
+
+// =   x=y x=y
+// +=  x += y        x = x + y
+// -=  x -= y        x = x - y
+// *=  x *= y        x = x * y
+// /=  x /= y        x = x / y
+// %=  x %= y        x = x % y
+
+// == igual a
+// === valor igual e igual, compara valor e tipo
+// ! = não é igual
+// !== não igual ou não igual
+// > maior que
+// < menor que
+// >= maior ou igual a
+// <= menor ou igual a
+
+// && e, compara dois valores e retorna true se ambos forem true
+// || ou 
+// Negativo
+
+// string so trabalha com adição para concatenar
+
+
+
+
+
+
+
+console.log() 
