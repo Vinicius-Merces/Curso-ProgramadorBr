cb0a1d484a1ab9b3ce6d9aa79dc094e41cd7ce6c
Vinicius-Merces <92902670+Vinicius-Merces@users.noreply.github.com>
2022-07-05 23:13:05 -0300

    Update script.js

--- Java/script.js
+++ Java/script.js
@@ -74,3 +74,43 @@
 // outro exemplo
 // var pode = idade >18 ? "pode" : "não pode!"
 // console.log(pode)
+
+// aplicação do switch
+
+// var nota1 = 3;
+// var nota2 = 5;
+
+// var media = (nota1 + nota2) /2;
+
+// var conceito = ""
+
+// if (media >=8){
+//     conceito = "Ótimo"
+// }
+
+// else if(media >=6.5) {
+//     conceito = "Bom"
+// }
+
+// else {
+//     conceito = "Abaixo da média"
+// }
+
+
+// console.log(media)
+// console.log(conceito)
+
+// switch(conceito){
+//     case "Ótimo":
+//         console.log("Parabéns, você é um ótimo aluno!");
+//         break;
+//     case "Bom":
+//         console.log("Você está quase perfeito");
+//         break;
+//     case "Abaixo da média" :
+//         console.log("Estude mais um pouco")
+//         break;
+//     default:
+//         console.log("Houve algum erro")
+// }
+
