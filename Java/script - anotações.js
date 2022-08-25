1ac78b3ee188a1b697f6fab73d4495632761c3d7
Vinicius-Merces <92902670+Vinicius-Merces@users.noreply.github.com>
2022-08-24 22:40:42 -0300

    Aulas de JS
    anotações e pratica

---
+++ Java/script - anotações.js
@@ -0,0 +1,116 @@
+// Boolean permite fazer operações lógicas (True e False)
+// Null é vazio
+// Undefined 
+// Number
+// String é uma cadeia de caracteres
+// + Adição
+// - Subtração
+// * Multiplicação
+//  / Divisão
+// % Módulo é para pegar o que resta da divisão, não gera número quebrado na divisão
+// ++ incremento
+// -- Decremento
+
+// =   x=y x=y
+// +=  x += y        x = x + y
+// -=  x -= y        x = x - y
+// *=  x *= y        x = x * y
+// /=  x /= y        x = x / y
+// %=  x %= y        x = x % y
+
+// == igual a
+// === valor igual e igual, compara valor e tipo
+// ! = não é igual
+// !== não igual ou não igual
+// > maior que
+// < menor que
+// >= maior ou igual a
+// <= menor ou igual a
+
+// && e, compara dois valores e retorna true se ambos forem true
+// || ou 
+// Negativo
+
+// string so trabalha com adição para concatenar
+
+// parseInt() transforma string em número inteiros
+// parseFloat() transforma string em números com casas decimais
+
+
+//Abaixo teste de prompt e alert
+
+// var nome = prompt("Qual seu nome?")
+
+// alert("Bom dia, " + nome) tomar cuidado com a virgula fora da string causa bug no código
+
+// var numero = parseInt (prompt("Digite um número"))
+// var dobro = numero + numero
+
+// alert("O dobro de " + numero + " é " + dobro)
+
+
+//teste de if e else
+// var idade = 36
+
+// if (idade >= 35){
+//     console.log("Pode")
+//     console.log("Qual o seu pedido?")
+// }
+
+// else if(idade >=18){
+//     console.log("pode")
+//     console.log("Mostre a identidade")
+// }
+// else{
+//     console.log("não pode")
+//     console.log("Volte futuramente")
+// }
+
+// Operador Ternário
+// var idade = 20
+
+// idade >= 18 ? console.log("Pode") : console.log("Não pode!")
+
+// outro exemplo
+// var pode = idade >18 ? "pode" : "não pode!"
+// console.log(pode)
+
+// aplicação do switch
+
+// var nota1 = 3;
+// var nota2 = 5;
+
+// var media = (nota1 + nota2) /2;
+
+// var conceito = ""
+
+// if (media >=8){
+//     conceito = "Ótimo"
+// }
+
+// else if(media >=6.5) {
+//     conceito = "Bom"
+// }
+
+// else {
+//     conceito = "Abaixo da média"
+// }
+
+
+// console.log(media)
+// console.log(conceito)
+
+// switch(conceito){
+//     case "Ótimo":
+//         console.log("Parabéns, você é um ótimo aluno!");
+//         break;
+//     case "Bom":
+//         console.log("Você está quase perfeito");
+//         break;
+//     case "Abaixo da média" :
+//         console.log("Estude mais um pouco")
+//         break;
+//     default:
+//         console.log("Houve algum erro")
+// }
+
