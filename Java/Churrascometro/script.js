ba30d628c27f5f4d31fd2772c8ef6d24fc5ee99d
Vinicius <92902670+Vinicius-Merces@users.noreply.github.com>
2022-08-30 12:06:52 -0300

    Projeto churrascometro
    Base do projeto montada seguindo a vídeo aula, falta implementar e melhorar o layout e UX

--- Java/Churrascometro/script.js
+++ Java/Churrascometro/script.js
@@ -1,19 +1,54 @@
-var numero = 0;
-var timer;
+// Carne - 400gr/pessoa + de 6 horas = 650gr/pessoa
+// Cerveja 1200ml/pessoa + de 6 horas = 2000ml/pessoa
+//Refrigerante/agua 1000ml + de 6 horas = 1500ml
 
-function AtualizarTexto(){
-    let h = document.getElementsByTagName("h1")[0];
-    h.innerHTML +=" " + numero;
-    numero++;
+// Crianças valem por 0,5
+
+let inputAdultos= document.getElementById("adultos");
+let inputCriancas= document.getElementById("criancas");
+let inputDuracao= document.getElementById("duracao");
+
+let resultado = document.getElementById("resultado");
+
+function calcular(){
+    console.log("Calculando...")
     
+    let adultos = inputAdultos.value;
+    let criancas = inputCriancas.value;
+    let duracao = inputDuracao.value;
+    
+
+    let qtdTotalCarne = carnepp(duracao) * adultos + (carnepp(duracao)/2 * criancas);
+    let qtdTotalCerveja = cervejapp(duracao) * adultos ;
+    let qtdTotalBebidas = bebidaspp(duracao) * adultos + (bebidaspp(duracao)/2 * criancas);
+    
+    resultado.innerHTML = `<p>${qtdTotalCarne/1000} Kg de Carne</p>`
+    resultado.innerHTML += `<p>${Math.ceil(qtdTotalCerveja/355)} Latas(355ml) de Cerveja</p>`
+    resultado.innerHTML += `<p>${Math.ceil(qtdTotalBebidas/2000)} Garrafas(2L) de Bebidas</p>`
+
 }
 
-function iniciar(){
-  
-   interval = setTimeout (AtualizarTexto, 3000);
+function carnepp(duracao){
+    let carne = 400;
+    if(duracao >= 6) {
+        return 650;
+    } else {
+        return 400;
+    }
 }
-
-function parar(){
-    clearTimeout(timer);
+function cervejapp(duracao){
+    let carne = 1200;
+    if(duracao >= 6) {
+        return 2000;
+    } else {
+        return 1200;
+    }
 }
-
+function bebidaspp(duracao){
+    let carne = 1000;
+    if(duracao >= 6) {
+        return 1500;
+    } else {
+        return 1000;
+    }
+}
