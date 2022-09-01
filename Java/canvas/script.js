32d310ab17f665d9e5e19aa74ea0aebfd4c924fd
Vinicius <92902670+Vinicius-Merces@users.noreply.github.com>
2022-09-01 11:39:52 -0300

    Add files via upload

---
+++ Java/canvas/script.js
@@ -0,0 +1,18 @@
+let tela = document.getElementById("tela");
+
+let ctx = tela.getContext("2d");
+
+ctx.rect(10,10, 100, 200);
+
+ctx.fillStyle = "blue";
+// ctx.fillRect(10,10, 100, 200);
+ctx.lineWidth = 5;
+ctx.strokeStyle = "red"
+
+ctx.fill();
+ctx.stroke();
+
+ctx.clearRect(20,20, 90,30);
+
+
+
--- Java/script.js
+++ Java/script.js
@@ -1,12 +1,1 @@
-var aluno = { nome: "Igor", notas: [7.5, 5.0]}
-
-
-var novaProp = "LastName"
-
-aluno.matricula = 12345;
-
-aluno[novaProp] = "Oliveira";
-
-console.log(aluno)
-
 
