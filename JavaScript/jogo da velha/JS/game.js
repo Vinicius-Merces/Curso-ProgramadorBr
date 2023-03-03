faf5a7fcf80e75bb4a8f16afaf6b8712c4dc197c
Vinicius-Merces <92902670+Vinicius-Merces@users.noreply.github.com>
2023-03-03 00:50:23 -0300

    Finalização do projeto
    Falta toques de layout

--- JavaScript/jogo da velha/JS/game.js
+++ JavaScript/jogo da velha/JS/game.js
@@ -2,18 +2,65 @@
 
 let board = ['', '', '', '', '', '', '', '', '',]
 let playerTime = 0;
-let symbols = ['o','x'];
+let symbols = ['o', 'x'];
+let gameOver = false;
+let winStates = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [0, 4, 8],
+    [2, 4, 6],
+]
 
-function handleMove (position){
+function handleMove(position) {
 
-    if(board[position] == '') {
-    board[position]= symbols[playerTime]
-
-    if (playerTime == 0){
-        playerTime = 1;
-    } else {
-        playerTime = 0;
+    if (gameOver) {
+        return;
     }
+
+    if (board[position] == '') {
+        board[position] = symbols[playerTime]
+
+        gameOver = isWin();
+
+        if (gameOver == false) {
+
+
+            playerTime = (playerTime == 0) ? 1 : 0;
+
+
+            //essa sequencia de if abaixo é a mesma coisa que o ternario acima
+            // if (playerTime == 0) {
+            //     playerTime = 1;
+            // } else {
+            //     playerTime = 0;
+            // }
+        }
+
+    }
+
+    return gameOver;
 }
 
+function isWin() {
+
+   
+    for (let i = 0; i < winStates.length; i++) {
+        let seq = winStates[i];
+
+        let pos1 = seq[0];
+        let pos2 = seq[1];
+        let pos3 = seq[2];
+
+        if (board[pos1] == board[pos2] &&
+            board[pos1] == board[pos3] &&
+            board[pos1] != '') {
+            return true;
+        }
+    }
+
+    return false;
 }
+
--- JavaScript/jogo da velha/JS/interface.js
+++ JavaScript/jogo da velha/JS/interface.js
@@ -1,35 +1,63 @@
-document.addEventListener('DOMContentLoaded', ()=> {
+document.addEventListener('DOMContentLoaded', () => {
 
     let squares = document.querySelectorAll(".square");
 
-    squares.forEach((square)=>{
+    squares.forEach((square) => {
         square.addEventListener('click', handleClick);
     })
 
 })
 
 
-function handleClick(event){
+function handleClick(event) {
 
     let square = event.target;
     let position = square.id;
 
-    handleMove(position);
-    updateSquares();
+    if (handleMove(position)) {
+
+        setTimeout(() => {
+            alert(" O Jogo Acabou! - O Vencedor foram as" + " " + playerTime);
+        }, 20);
+
+
+
+        if (playerTime == 0) {
+            playerTime = "Abóboras"
+        }
+        else {
+            playerTime = "Caveiras"
+        }
+
+
+    };
+    updateSquare(position);
 }
 
-function updateSquares(){
+function updateSquare(position) {
+    let square = document.getElementById(position.toString());
+    let symbol = board[position];
+    square.innerHTML = `<div class='${symbol}'></div>`
+}
+
+let resetButton = document.getElementById("reset-button");
+resetButton.addEventListener("click", resetGame);
+
+function resetGame() {
+    // redefine as variáveis relevantes do jogo
+    board = ["", "", "", "", "", "", "", "", ""];
+    gameOver = false;
+    playerTime = 0;
+    symbols = ["o", "x"];
+
+    // redefine o conteúdo das quadrículas para o estado inicial
+    resetSquares();
+}
+function resetSquares() {
     let squares = document.querySelectorAll(".square");
 
-    squares.forEach((square)=>{
-        let position = square.id;
-        let symbol = board[position];
-
-        if(symbol != '') {
-            square.innerHTML =`<div class='${symbol}'></div>`
-        }
-    })
+    squares.forEach((square) => {
+        // let position = square.id;
+        square.innerHTML = `<div class=''></div>`;
+    });
 }
-
-
-
