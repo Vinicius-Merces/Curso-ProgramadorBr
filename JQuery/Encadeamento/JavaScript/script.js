9980c52d3381672a90768544e24041eb5d37aa63
Vinicius-Merces <92902670+Vinicius-Merces@users.noreply.github.com>
2023-03-31 01:18:16 -0300

    Teste com encadeamento simples

--- JQuery/Encadeamento/JavaScript/script.js
+++ JQuery/Encadeamento/JavaScript/script.js
@@ -1,2 +1,19 @@
-let jq = $();
+let jq = $("#lista1").slideUp(2000)
+// jq.slideDown(2000)
+
+function animar() {
+    jq.slideDown(2000) 
+      .fadeOut(2000)
+      .fadeIn(2000)
+      .slideUp(1000)
+      
+               
+} 
+
+
+setInterval(animar, 6000);
+
+
+console.log(jq);
+
 
